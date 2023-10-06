860437709c2ae7450cca037702a55db53d4e41e4
alurtaher <117931556+alurtaher@users.noreply.github.com>
2023-10-06 14:32:54 +0530

    querySelector

--- javas.js
+++ javas.js
@@ -6,6 +6,8 @@ addItems.innerHTML='ADD ITEMS';
 addItems.style.fontWeight='bold';
 addItems.style.color='green';
 var items=document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor='green';
-items[2].style.fontWeight='bold';
+//items[2].style.backgroundColor='green';
+//items[2].style.fontWeight='bold';
+items[1].style.backgroundColor='green';
+items[2].style.color='transparent';
 
