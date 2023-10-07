364bc490bd4119f8fd847d56700f0f967e3959b9
alurtaher <117931556+alurtaher@users.noreply.github.com>
2023-10-07 11:32:31 +0530

    adding functionality

--- javas.js
+++ javas.js
@@ -1,67 +1,45 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
+// Define your list of items (name and description)
+const items = [
+  { name: "Item 1", description: "This is item 1 description." },
+  { name: "Item 2", description: "Description for item 2." },
+  // Add more items here
+];
 
-// Form submit event
-form.addEventListener('submit', addItem);
-// Delete event
-itemList.addEventListener('click', removeItem);
-// Filter event
-filter.addEventListener('keyup', filterItems);
+// Get references to the input box and item list
+const searchInput = document.getElementById("searchInput");
+const itemList = document.getElementById("itemList");
 
-// Add item
-function addItem(e){
-  e.preventDefault();
+// Add event listener for input changes
+searchInput.addEventListener("input", updateItemList);
 
-  // Get input value
-  var newItem = document.getElementById('item').value;
+// Function to update the displayed item list based on the search query
+function updateItemList() {
+  const query = searchInput.value.toLowerCase();
 
-  // Create new li element
-  var li = document.createElement('li');
-  // Add class
-  li.className = 'list-group-item';
-  // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+  // Clear the current list
+  itemList.innerHTML = "";
 
-  // Create del button element
-  var deleteBtn = document.createElement('button');
+  // Filter and display matching items
+  items.forEach(item => {
+      const itemName = item.name.toLowerCase();
+      const itemDescription = item.description.toLowerCase();
 
-  // Add classes to del button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+      if (itemName.includes(query) || itemDescription.includes(query)) {
+          // Create list item and description element
+          const listItem = document.createElement("li");
+          const description = document.createElement("p");
+          
+          listItem.textContent = item.name;
+          description.textContent = item.description;
 
-  // Append text node
-  deleteBtn.appendChild(document.createTextNode('X'));
+          // Append the description below the item name
+          listItem.appendChild(description);
 
-  // Append button to li
-  li.appendChild(deleteBtn);
-
-  // Append li to list
-  itemList.appendChild(li);
-}
-
-// Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
-    }
-  }
-}
-
-// Filter Items
-function filterItems(e){
-  // convert text to lowercase
-  var text = e.target.value.toLowerCase();
-  // Get lis
-  var items = itemList.getElementsByTagName('li');
-  // Convert to an array
-  Array.from(items).forEach(function(item){
-    var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
+          // Append the list item to the list
+          itemList.appendChild(listItem);
+      }
   });
 }
+
+// Initial update of the item list
+updateItemList();
